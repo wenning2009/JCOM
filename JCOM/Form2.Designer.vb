9ef79b4ef8e089e19c4f4d86f9e6e2d93f3b9338
wenning2009 <wenning2009@126.com>
2017-05-11 14:12:33 +0800

    test2

---
+++ JCOM/Form2.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BbLabelタイトル1 = New BB.BBLabelタイトル()
+        Me.BbLabelタイトル2 = New BB.BBLabelタイトル()
+        Me.SuspendLayout()
+        '
+        'BbLabelタイトル1
+        '
+        Me.BbLabelタイトル1.AutoSize = True
+        Me.BbLabelタイトル1.BBメッセージ表示名 = Nothing
+        Me.BbLabelタイトル1.Location = New System.Drawing.Point(0, 42)
+        Me.BbLabelタイトル1.Name = "BbLabelタイトル1"
+        Me.BbLabelタイトル1.Size = New System.Drawing.Size(87, 12)
+        Me.BbLabelタイトル1.TabIndex = 0
+        Me.BbLabelタイトル1.Text = "BbLabelタイトル1"
+        Me.BbLabelタイトル1.UseMnemonic = False
+        '
+        'BbLabelタイトル2
+        '
+        Me.BbLabelタイトル2.AutoSize = True
+        Me.BbLabelタイトル2.BBメッセージ表示名 = Nothing
+        Me.BbLabelタイトル2.Location = New System.Drawing.Point(99, 125)
+        Me.BbLabelタイトル2.Name = "BbLabelタイトル2"
+        Me.BbLabelタイトル2.Size = New System.Drawing.Size(87, 12)
+        Me.BbLabelタイトル2.TabIndex = 1
+        Me.BbLabelタイトル2.Text = "BbLabelタイトル2"
+        Me.BbLabelタイトル2.UseMnemonic = False
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.BbLabelタイトル2)
+        Me.Controls.Add(Me.BbLabelタイトル1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BbLabelタイトル1 As BB.BBLabelタイトル
+    Friend WithEvents BbLabelタイトル2 As BB.BBLabelタイトル
+End Class
