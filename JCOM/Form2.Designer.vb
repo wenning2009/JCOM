d292c8681da0484c6225130e2c5c6db8eb47dbed
wenning2009 <wenning2009@126.com>
2017-05-11 14:14:56 +0800

    test2

--- JCOM/Form2.Designer.vb
+++ JCOM/Form2.Designer.vb
@@ -24,6 +24,8 @@ Partial Class Form2
     Private Sub InitializeComponent()
         Me.BbLabelタイトル1 = New BB.BBLabelタイトル()
         Me.BbLabelタイトル2 = New BB.BBLabelタイトル()
+        Me.BbLabelタイトル3 = New BB.BBLabelタイトル()
+        Me.BbLabelタイトル4 = New BB.BBLabelタイトル()
         Me.SuspendLayout()
         '
         'BbLabelタイトル1
@@ -48,11 +50,35 @@ Partial Class Form2
         Me.BbLabelタイトル2.Text = "BbLabelタイトル2"
         Me.BbLabelタイトル2.UseMnemonic = False
         '
+        'BbLabelタイトル3
+        '
+        Me.BbLabelタイトル3.AutoSize = True
+        Me.BbLabelタイトル3.BBメッセージ表示名 = Nothing
+        Me.BbLabelタイトル3.Location = New System.Drawing.Point(99, 183)
+        Me.BbLabelタイトル3.Name = "BbLabelタイトル3"
+        Me.BbLabelタイトル3.Size = New System.Drawing.Size(87, 12)
+        Me.BbLabelタイトル3.TabIndex = 2
+        Me.BbLabelタイトル3.Text = "BbLabelタイトル3"
+        Me.BbLabelタイトル3.UseMnemonic = False
+        '
+        'BbLabelタイトル4
+        '
+        Me.BbLabelタイトル4.AutoSize = True
+        Me.BbLabelタイトル4.BBメッセージ表示名 = Nothing
+        Me.BbLabelタイトル4.Location = New System.Drawing.Point(131, 42)
+        Me.BbLabelタイトル4.Name = "BbLabelタイトル4"
+        Me.BbLabelタイトル4.Size = New System.Drawing.Size(87, 12)
+        Me.BbLabelタイトル4.TabIndex = 3
+        Me.BbLabelタイトル4.Text = "BbLabelタイトル4"
+        Me.BbLabelタイトル4.UseMnemonic = False
+        '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.BbLabelタイトル4)
+        Me.Controls.Add(Me.BbLabelタイトル3)
         Me.Controls.Add(Me.BbLabelタイトル2)
         Me.Controls.Add(Me.BbLabelタイトル1)
         Me.Name = "Form2"
@@ -63,4 +89,6 @@ Partial Class Form2
     End Sub
     Friend WithEvents BbLabelタイトル1 As BB.BBLabelタイトル
     Friend WithEvents BbLabelタイトル2 As BB.BBLabelタイトル
+    Friend WithEvents BbLabelタイトル3 As BB.BBLabelタイトル
+    Friend WithEvents BbLabelタイトル4 As BB.BBLabelタイトル
 End Class
--- JCOM/Form2.vb
+++ JCOM/Form2.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form2
 
+    Private Sub BbLabelタイトル1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BbLabelタイトル1.Click
+
+    End Sub
 End Class
